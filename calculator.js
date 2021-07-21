aa9aa145362853885e96856b59a362184528911d
Usman Suleiman <usmansbk@gmail.com>
2021-07-21 01:50:31 +0100

    Subtraction passing

--- calculator.js
+++ calculator.js
@@ -2,6 +2,10 @@ function Calculator() {
   this.add = function (a, b) {
     return a + b;
   };
+
+  this.subtract = function (a, b) {
+    return a - b;
+  };
 }
 
 module.exports = Calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -25,3 +25,29 @@ describe("add", () => {
     expect(answer).toBe(-2);
   });
 });
+
+describe("subtract", () => {
+  test("1 - 1", () => {
+    const calculator = new Calculator();
+
+    const answer = calculator.subtract(1, 1);
+
+    expect(answer).toBe(0);
+  });
+
+  test("1 - -1", () => {
+    const calculator = new Calculator();
+
+    const answer = calculator.subtract(1, -1);
+
+    expect(answer).toBe(2);
+  });
+
+  test("0 - 1", () => {
+    const calculator = new Calculator();
+
+    const answer = calculator.subtract(0, -1);
+
+    expect(answer).toBe(1);
+  });
+});
